8cf010f9e2cc012dc4ee37984c26dd5fb04dd28e
VGA_Info <66370371+vga-info@users.noreply.github.com>
2024-07-22 21:38:08 -0300

    aula 5

---
+++ CursoemVideo/desafio03.py
@@ -0,0 +1,5 @@
+print ('===== DESAFIO 03 =====')
+num1 = int(input ('Digite um número: '))
+num2 = int(input ('Digite outro número: '))
+soma = num1 + num2
+print ('A soma dos números é: ', soma)
--- desafio03.py
+++
@@ -1,5 +0,0 @@
-print ('===== DESAFIO 03 =====')
-num1 = int(input ('Primeiro Número: '))
-num2 = int(input ('Segundo Número: '))
-total = num1 + num2
-print ('A soma dos números é: ', total)
