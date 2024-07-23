e1c1999875b16e5450234bff17b5cd3ab20e5f17
VGA_Info <66370371+vga-info@users.noreply.github.com>
2024-07-22 21:55:25 -0300

    inicio de pythonTeste

---
+++ CursoemVídeo/ex001.py
@@ -0,0 +1,3 @@
+print('Olá Mundo! 1')
+msg='Olá Mundo! 2'
+print(msg)
---
+++ CursoemVídeo/ex002.py
@@ -0,0 +1,5 @@
+nome = input('Qual seu nome? ')
+print('É um prazer te conhecer', nome, '.')
+print('ou')
+print('É um prazer te conhecer, {}.'.format(nome))
+#{} são substituidas pelo valor da variavel especificada em .format
