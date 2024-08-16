758cad04e6a64dc9cf5a9c9a9f3fc6ae142c8bac
VGA_Info <66370371+vga-info@users.noreply.github.com>
2024-08-16 16:16:35 -0300

    Correção Desafios 053 e 054
    Finalização da correção dos desafios 053(modelo B) e 054

--- CursoemVideo/desafio053b.py
+++ CursoemVideo/desafio053b.py
@@ -7,10 +7,7 @@
 frase = str(input('Digite uma frase: ')).strip().upper()
 palavras = frase.split()
 junto = ''.join(palavras)
-inverso = ''
-for letra in range(len(junto) -1, -1, -1):
-    inverso += junto[letra]
+inverso = junto[::-1]
+print('O inverso de {} é {}'.format(junto, inverso))
 if inverso == junto:
-    print('Temos um palíndromo!')
-else:
-    print('A frase nao é um palíndromo.')
+    print('Temos um Palíndromo!')
--- CursoemVideo/desafio054.py
+++ CursoemVideo/desafio054.py
@@ -7,15 +7,19 @@ import datetime
 
 # Contador para pessoas menores de 21 anos
 menor_de_idade = 0
+maior_de_idade = 0
 
 # Loop para ler o ano de nascimento de 7 pessoas
 for c in range(1, 8):
-    nasc = int(input('Digite o ano de nascimento: '))
+    nasc = int(input('Em que ano a {}ª pessoa nasceu: '.format(c)))
     idade = datetime.date.today().year - nasc
 
     # Verifica se a idade é menor que 21
     if idade < 21:
         menor_de_idade += 1
-
+    if idade > 20:
+        maior_de_idade += 1
 # Exibe o número de pessoas que ainda não atingiram a maioridade
 print(f'Número de pessoas que ainda não atingiram a maioridade: {menor_de_idade}')
+print(f'Numero de pessoas que já atingiram a maioridade: {maior_de_idade}')
+
