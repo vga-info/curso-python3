28ce4def32e0d1eb7ed0e844009171aac4506b6d
VGA_Info <66370371+vga-info@users.noreply.github.com>
2024-08-22 17:47:58 -0300

    Desafios 061 e 062
    Desafio 061 pronto, desafio 062 em execução.

--- CursoemVideo/desafio061.py
+++ CursoemVideo/desafio061.py
@@ -1,4 +1,15 @@
-#desafio061
-# refaça o desafio 051 , lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros
-# termos da progressão usando a estrutura while.
+# DESAFIO 061
+# Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA,
+# mostrando os 10 primeiros termos da progressão usando a estrutura while.
 
+primeiro_termo = int(input('Digite o primeiro termo da PA: '))  # Solicita o primeiro termo da PA
+razao = int(input('Digite a razão da PA: '))  # Solicita a razão da PA
+
+numtermos = 0  # Inicializa o contador de termos
+
+while numtermos != 10:  # Continua o loop até gerar 10 termos
+    termo_atual = primeiro_termo + numtermos * razao  # Calcula o termo atual da PA
+    print(termo_atual, end=' ')  # Exibe o termo atual seguido por um espaço
+    numtermos += 1  # Incrementa o contador de termos
+
+print()  # Adiciona uma linha ao final da saída
--- CursoemVideo/desafio062.py
+++ CursoemVideo/desafio062.py
@@ -1,3 +1,24 @@
 # desafio 062
 # Melohore o desafio 061, perguntando para o usuario se ele quer mostrar mais alguns termos.
 # O programa encerra quando ele disser que quer mostrar 0 termos.
+
+primeiro_termo = int(input('Digite o primeiro termo da PA: '))  # Solicita o primeiro termo da PA
+razao = int(input('Digite a razão da PA: '))  # Solicita a razão da PA
+
+numtermos = 0  # Inicializa o contador de termos
+continuar = 1
+while numtermos != 10:  # Continua o loop até gerar 10 termos
+    termo_atual = primeiro_termo + numtermos * razao  # Calcula o termo atual da PA
+    print(termo_atual, end=' ')  # Exibe o termo atual seguido por um espaço
+    numtermos += 1  # Incrementa o contador de termos
+
+while continuar != 0:
+    continuar = int(input('\n\nDeseja continuar mostrando?\nDigite a Qtd de termos adicionais\n ou 0 (zero) para parar: '))
+    continua_pa = continuar + numtermos
+    while numtermos != continua_pa:  # Continua o loop até gerar 10 termos
+            termo_atual = primeiro_termo + numtermos * razao  # Calcula o termo atual da PA
+            print(termo_atual, end=' ')  # Exibe o termo atual seguido por um espaço
+            numtermos += 1  # Incrementa o contador de termos
+    continuar -= 1
+print('Saindo do programa...\n')
+print('Fim.')  # Adiciona uma linha ao final da saída
